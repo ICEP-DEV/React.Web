e09bf01811fae29dd1cad93bc775b5c494f1d4c1
Dipono Joel Manasoe <ManasoeDJ@tut.ac.za>
2024-07-23 10:27:27 +0200

    updated

--- src/screens/Employee.jsx
+++ src/screens/Employee.jsx
@@ -12,6 +12,7 @@ function Employee() {
     ];
     return (<div >
         <Header />
+        
         <table id="customers">
             <thead>
                 <th>Id</th>
@@ -28,7 +29,7 @@ function Employee() {
                         <td>{emp.Location}</td>
                         <td>{emp.Salary}</td>
                         <td>
-                            <button className="btn btn-primary">Edit</button>
+                            <button className="btn btn-success">Edit</button>
                             <button className="btn btn-danger">Delete</button>
                         </td>
                     </tr>
