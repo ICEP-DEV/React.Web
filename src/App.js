b54570e252de15d1ce8b3fafaa69b77b89e85509
Dipono Joel Manasoe <ManasoeDJ@tut.ac.za>
2024-07-23 10:00:16 +0200

    Employees list file added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Register from './screens/Register';
 import Login from './screens/Login'
-
+import Employee from './screens/Employee'
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Register />} />
-        <Route exact path='/login' element={<Login />} />
-
+        {/* <Route exact path='/' element={<Register />} />
+        <Route exact path='/login' element={<Login />} /> */}
+        
+        <Route exact path='/' element={<Employee />} />
       </Routes>
     </Router>
   );
---
+++ src/screens/Employee.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+
+function Employee() {
+    const employees = [
+
+        { Id: 101, Name: 'Abhinav', Location: 'Bangalore', Salary: 12345 },
+
+        { Id: 102, Name: 'Abhishek', Location: 'Chennai', Salary: 23456 },
+
+        { Id: 103, Name: 'Ajay', Location: 'Bangalore', Salary: 34567 }
+
+    ];
+
+
+    return (<div >
+        <table id="customers">
+            <thead>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Salary</th>
+            </thead>
+            <tbody>
+                {employees.map((emp, xid) => (
+                    <tr key={xid}>
+                        <td>{emp.Id}</td>
+                        <td>{emp.Name}</td>
+                        <td>{emp.Location}</td>
+                        <td>{emp.Salary}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+
+
+    </div>)
+
+
+}
+
+export default Employee;
